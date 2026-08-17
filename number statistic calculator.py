0ebdbf66054a10a705318082226345fa2803ef8d
sanjanagupta8595 <sanjana76479@gmail.com>
2026-08-17 22:32:59 +0530

    update code

--- number statistic calculator.py
+++ number statistic calculator.py
@@ -4,6 +4,8 @@ print(topic.center(50))
 n = int(input("HOW MANY NUMBERS DO YOU WANT TO ENTER : "))
 total =0
 average = 0
+even_num = 0
+odd_num = 0
 for i in range(1,n+1):
         val = int(input("Enter NUMBER : "))
         total = total+val
@@ -16,14 +18,20 @@ for i in range(1,n+1):
                 maximum = val
             if val < minimum:
                 minimum = val
+
+
         if val%2==0:
-            print("even number ")
-        else :
-            print("odd number ")
+                  even_num+=1
+        else:
+             odd_num+=1
+     
 print ("total : ",total)
 print("average : ",average)
 print("maximum number : ",maximum)
 print("minimum number : ",minimum)
-print("SUCCESSFULL!!!")
+print("Even Number : ", even_num)
+print("Odd Number : ",odd_num)
 
+
+print("SUCCESSFULL!!!")
        
